9d199e8319e26ad0bbec3ae1d8c4d5786024ee42
wang-local <wang-local@local06.smg.nii.ac.jp>
2019-12-18 16:53:30 +0900

    1. Update 04_tar_prj.sh for making tar ball; 2. Update README

--- waveform-modeling/project-NSF/04_tar_prj.sh
+++ waveform-modeling/project-NSF/04_tar_prj.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 # ----- This script make a tar ball of this project,
-# ----- without unnecessary files
 
 PRJNAME=`basename $PWD`
 cd ../
-tar --exclude="./${PRJNAME}/*DATATEMP*" --exclude="./${PRJNAME}/MODELS/*/epoch*" -cvzf ${PRJNAME}.tar.gz ./SCRIPTS ./${PRJNAME}
-cd ${PRJNAME}
-mv ../${PRJNAME}.tar.gz ./
+SUPPRJNAME=`basename $PWD`
+cd ../
+tar --exclude="./${SUPPRJNAME}/${PRJNAME}/*DATATEMP*" --exclude="./${SUPPRJNAME}/${PRJNAME}/MODELS/*/epoch*" --exclude="./${SUPPRJNAME}/${PRJNAME}/*.tar.gz" -cvzf ${PRJNAME}.tar.gz ./${SUPPRJNAME}/SCRIPTS ./init.sh ./${SUPPRJNAME}/${PRJNAME}
+cd ./${SUPPRJNAME}/${PRJNAME}
+mv ../../${PRJNAME}.tar.gz ./
+
+echo "Please find the tar ball: ${PRJNAME}.tar.gz"

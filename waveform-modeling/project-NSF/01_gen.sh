37cd1c14b5ceabaef5ad3289704d0a7a30fac48a
TonyWangX <wangxin@nii.ac.jp>
2020-02-09 15:08:50 +0900

    Modify 01_gen.sh to replace environment variable

--- waveform-modeling/project-NSF/01_gen.sh
+++ waveform-modeling/project-NSF/01_gen.sh
@@ -4,16 +4,16 @@
 export TEMP_WAVEFORM_MODEL_INPUT_DIRS=$PWD/../TESTDATA-for-pretrained/mfbsp,$PWD/../TESTDATA-for-pretrained/f0
 
 # Path to the model directory
-export TEMP_WAVEFORM_MODEL_DIRECTORY=$PWD/MODELS/NSF
+export TEMP_WAVEFORM_MODEL_DIRECTORY=$PWD/MODELS/h-sinc-NSF
 
 # Path to the directory that will save the generated waveforms
-export TEMP_WAVEFORM_OUTPUT_DIRECTORY=$PWD/MODELS/NSF/output_trained
+export TEMP_WAVEFORM_OUTPUT_DIRECTORY=${TEMP_WAVEFORM_MODEL_DIRECTORY}/output_trained
 
 # Path to the trained_network.jsn (or epoch*.autosave)
-export TEMP_WAVEFORM_MODEL_NETWORK_PATH=$PWD/MODELS/NSF/trained_network.jsn
+export TEMP_WAVEFORM_MODEL_NETWORK_PATH=${TEMP_WAVEFORM_MODEL_DIRECTORY}/trained_network.jsn
 
 # Path to a temporary directory to save intermediate files (which will be deleted after generation)
-export TEMP_WAVEFORM_TEMP_OUTPUT_DIRECTORY=$PWD/MODELS/NSF/output_trained_tmp
+export TEMP_WAVEFORM_TEMP_OUTPUT_DIRECTORY=${TEMP_WAVEFORM_MODEL_DIRECTORY}/output_trained_tmp
 
 # generating
 python ../SCRIPTS/02_genwaveform.py config
